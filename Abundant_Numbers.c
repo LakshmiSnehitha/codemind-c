fdfec81e140657caf9c3aeae36f4144b4a5a0c4a
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2022-12-14 09:52:53 +0530

    #include<stdio.h> int main() {     int n,i,fact=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {     if(n%i==0)     {         fact=fact+i;     }     }     if(fact>n)    {         printf("True");    }    else    {        printf("False");    } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fact=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+    if(n%i==0)
+    {
+        fact=fact+i;
+    }
+    }
+    if(fact>n)
+   {
+        printf("True");
+   }
+   else
+   {
+       printf("False");
+   }
+}
